8a1f6c4dfac5f610edac01790406e746cabb112a
acostacion <carmengomezb27@gmail.com>
2025-10-10 10:43:50 +0200

    LEER DESCRIPCION
    - hacer que en vez de PxVec3 se use Vector3D
    - ir rastreando TODOs e ir haciéndolos.

--- skeleton/src/ParticleGen.h
+++ skeleton/src/ParticleGen.h
@@ -2,10 +2,14 @@
 #include <random>
 class ParticleGen
 {
+public:
 	virtual void generateP() = 0;
 
 	// TODO: setters y getters pos, vel, dir, probGen
 	std::mt19937 _mt;
 	std::uniform_real_distribution<double> _u{ 0,1 };	
+
+private:
+
 };
 
